8457b222efd6fb6b2c7fb00d22af55d4bd8ab16d
Sam Wells <samwells95@gmail.com>
2021-02-22 18:36:48 +0000

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders Hi message with emoji', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hi 👋/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Renders Name message', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/I'm Sam Wells/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
