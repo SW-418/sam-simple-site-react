5dee5db4bb573e1f95a6c6285e05d7ac6c74ed48
Sam Wells <samwells95@gmail.com>
2021-09-10 12:22:26 +0100

    Link and package updates

--- src/Components/SocialMedia/Buttons/LinkedInButton.js
+++ src/Components/SocialMedia/Buttons/LinkedInButton.js
@@ -5,7 +5,7 @@ import './SocialMediaButtons.css';
 function LinkedInButton() {
     return (
         <div class="social-media-follow-btn">
-            <a href="https://www.linkedin.com/in/samwells182/">
+            <a href="https://www.linkedin.com/in/sw-418/">
                 <img src={LinkedInIcon} alt="Connect with Sam Wells on LinkedIn" />
             </a>
         </div>
