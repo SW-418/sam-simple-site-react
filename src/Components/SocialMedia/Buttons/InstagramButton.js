7444c0a74b43e9ede832322f942291f530b307ef
Sam Wells <samwells95@gmail.com>
2021-11-02 14:55:54 -0700

    update insta xoxo

--- src/Components/SocialMedia/Buttons/InstagramButton.js
+++ src/Components/SocialMedia/Buttons/InstagramButton.js
@@ -5,7 +5,7 @@ import './SocialMediaButtons.css';
 function InstagramButton() {
     return (
         <div class="social-media-follow-btn">
-            <a href="https://www.instagram.com/_samwells95/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/__sam.wells/" target="_blank" rel="noopener noreferrer">
                 <img src={InstagramIcon} alt="Follow Sam Wells on Instagram" />
             </a>
         </div>
